cccbaff16ada747918aad1b62262358815e4e9e8
nithishccc <114800669+nithishccc@users.noreply.github.com>
2022-10-12 07:31:20 -0700

    Add files via upload

---
+++ PYTH.GIT/add.py.py
@@ -0,0 +1,8 @@
+#add.py
+    #create two variables (x and y) and assign values to them
+    #create a third variable(sum) and add x and y
+    #print the su,
+x=3
+y=6
+sum=x+y
+print("the answer is" ,sum)
---
+++ PYTH.GIT/swapfunction.py
@@ -0,0 +1,22 @@
+from tkinter import Y
+
+
+x=5
+y=10
+temp=5
+print(x)
+print(y)
+print('the value of temp is :' ,x)
+
+x=5
+y=10
+print('before swap, value of x is :',x)
+print('before swap, value of y is:',y)
+temp=x
+print('value of temp is :',y)
+x=y 
+print('value of x is :',x)
+y=temp
+print("value of y is :,y")
+print("after swap,value of x is ",x)
+print('after swap,value of y is ',y)
